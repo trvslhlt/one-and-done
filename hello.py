a96291943dc4888119a918640d7f8ca2fc02894c
trvslhlt <trvslhlt@gmail.com>
2016-11-13 15:24:51 -0500

    provision to live hello world

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
-
-# route goes here
 @app.route("/")
 def hello():
     return "Hello world!"
--- provision.sh
+++ provision.sh
@@ -6,6 +6,35 @@ set -x
 
 echo "provisioning!"
 
-# TODO your code here
+#sudo python3 -m http.server 80 &
+
+sudo apt-get update 
+sudo apt-get -y upgrade
+yes | sudo apt-get install nginx
+yes | sudo apt-get install python-pip
+
+sudo mkdir -p /var/www
+sudo mkdir -p /var/www/hello_world
+# sudo chown -R ubuntu:ubuntu /var/www/hello_world
+cd /var/www/hello_world
+
+sudo cp /vagrant/hello.py hello.py
+sudo cp /vagrant/nginx_config nginx_config
+
+yes | sudo apt-get install python-virtualenv
+virtualenv venv
+. venv/bin/activate
+
+yes | sudo pip install flask
+pip freeze > requirements.txt
+
+yes | sudo pip install gunicorn
+gunicorn hello:app -p hello.pid -b 0.0.0.0:8000 -D
+
+sudo cp nginx_config /etc/nginx/sites-available/nginx_config
+sudo ln -s \
+    /etc/nginx/sites-available/nginx_config \
+    /etc/nginx/sites-enabled/nginx_config
 
 echo "provisioning complete!"
+
